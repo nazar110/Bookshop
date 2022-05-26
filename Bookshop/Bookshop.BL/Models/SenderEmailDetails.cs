c978a0846c320104e3261038e0bc2ac10521fd49
n.karas <n.karas@brainence.com>
2022-05-26 05:30:33 +0300

    Added some business logic

---
+++ Bookshop/Bookshop.BL/Models/SenderEmailDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.BL.Models
+{
+    public class SenderEmailDetails
+    {
+        public string SenderEmail { get; set; }
+        public string SenderPassword { get; set; }
+        public string RecipientEmail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Bookshop/Bookshop.BL/Services/NotificationService.cs
+++ Bookshop/Bookshop.BL/Services/NotificationService.cs
@@ -1,4 +1,7 @@
 ﻿using Bookshop.BL.DTOs;
+using Bookshop.BL.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,27 @@ namespace Bookshop.BL.Services
 {
     public class NotificationService
     {
-        void Send(ClientReadDto client)
+        public async void Send(SenderEmailDetails senderEmailDetails)
         {
+            var emailMessage = new MimeMessage();
 
+            emailMessage.From.Add(new MailboxAddress("Bookshop", senderEmailDetails.SenderEmail));
+            emailMessage.To.Add(new MailboxAddress("", senderEmailDetails.RecipientEmail));
+            emailMessage.Subject = senderEmailDetails.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = senderEmailDetails.Message
+            };
+
+            using (var client2 = new SmtpClient())
+            {
+                await client2.ConnectAsync("smtp.gmail.com", 465, true);
+                await client2.AuthenticateAsync(senderEmailDetails.SenderEmail, senderEmailDetails.SenderPassword);
+                await client2.SendAsync(emailMessage);
+
+                await client2.DisconnectAsync(true);
+            }
         }
+
     }
 }
